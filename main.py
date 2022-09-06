58df30c4d4c54141d63e2e0e0cbd8fe9c60b3fe4
bryantkrause <bryantkrause@gmail.com>
2022-09-06 16:26:47 -0700

    need more data

---
+++ main.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import requests
+import json
+import prettytable
+
+headers = {'Content-type': 'application/json'}
+data = json.dumps({"seriesid": ['CUUR0000SA0','SUUR0000SA0'],"startyear":"2011", "endyear":"2014"})
+p = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
+json_data = json.loads(p.text)
+for series in json_data['Results']['series']:
+    x=prettytable.PrettyTable(["series id","year","period","value","footnotes"])
+    seriesId = series['seriesID']
+    for item in series['data']:
+        year = item['year']
+        period = item['period']
+        value = item['value']
+        footnotes=""
+        for footnote in item['footnotes']:
+            if footnote:
+                footnotes = footnotes + footnote['text'] + ','
+        if 'M01' <= period <= 'M12':
+            x.add_row([seriesId,year,period,value,footnotes[0:-1]])
+    output = open(seriesId + '.txt','w')
+    output.write (x.get_string())
+    output.close()
+
+# example 2 https://towardsdatascience.com/acquire-and-analyze-inflation-data-in-the-us-with-an-api-python-and-tableau-ae81944bcbb0
